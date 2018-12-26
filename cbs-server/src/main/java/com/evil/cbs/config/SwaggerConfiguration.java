98421cc787f47f3f83d18a6b49b14cf6af9f7785
Ion Pascari <ionpascari1995@gmail.com>
2018-12-27 00:52:36 +0200

    Added Swagger Configuration

---
+++ cbs-server/src/main/java/com/evil/cbs/config/SwaggerConfiguration.java
@@ -0,0 +1,62 @@
+package com.evil.cbs.config;
+
+import com.fasterxml.classmate.TypeResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration extends WebMvcConfigurationSupport {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Cinema Booking System",
+                "This application will automate the reservation of tickets and enquiries about availability of the tickets for specific movies.",
+                "0.0.1 SNAPSHOT",
+                "Terms of service",
+                new Contact("Ion Pascari", "https://thefaustus.github.io/cinema-booking-system/", "ionpascari1995@gmail.com"),
+                "MIT License", "https://github.com/theFaustus/cinema-booking-system/blob/master/LICENSE", Collections.emptyList());
+    }
+
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public TypeResolver typeResolver() {
+        return new TypeResolver();
+    }
+
+    @Bean
+    public RequestMappingHandlerMapping requestMappingHandlerMapping() {
+        return new RequestMappingHandlerMapping();
+    }
+}
